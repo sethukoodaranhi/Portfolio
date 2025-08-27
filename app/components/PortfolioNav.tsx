32f069bb0b64f2229ae001af403e1b97b233ecb6
sethulakshmi <sethukoodaranhi@gmail.com>
2025-08-27 22:26:29 +0530

    nav changes added

--- app/components/PortfolioNav.tsx
+++ app/components/PortfolioNav.tsx
@@ -13,15 +13,15 @@ function PortfolioNav() {
     return (
         <Navbar expand="md" className={`${styles.portfolioNavbar} ${sen.className}`} sticky="top">
             <Container>
-                <Navbar.Brand href="#home">portfolio</Navbar.Brand>
+                <Navbar.Brand href="#home" >portfolio</Navbar.Brand>
                 <Navbar.Toggle aria-controls="portfolio-navbar-nav" className={`${styles.toggleIcon}`}/>
                 <Navbar.Collapse id="portfolio-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link as={Link} href="#home">Home</Nav.Link>
-                        <Nav.Link as={Link}href="#about">About</Nav.Link>
-                        <Nav.Link as={Link} href="#skill">Skill</Nav.Link>
-                        <Nav.Link as={Link} href="#projects">Project</Nav.Link>
-                        <Nav.Link as={Link} href="#contact">Contact</Nav.Link>
+                    <Nav className="ms-auto" defaultActiveKey={"#home"}>
+                        <Nav.Link as={Link} href="#home" eventKey="#home">Home</Nav.Link>
+                        <Nav.Link as={Link}href="#about" eventKey="#about">About</Nav.Link>
+                        <Nav.Link as={Link} href="#skill" eventKey="#skill">Skill</Nav.Link>
+                        <Nav.Link as={Link} href="#projects" eventKey="#project">Project</Nav.Link>
+                        <Nav.Link as={Link} href="#contact" eventKey="#contact">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
