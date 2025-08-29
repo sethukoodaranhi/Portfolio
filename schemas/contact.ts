f59edcb90ed7522801973194d243ebdf3540da9b
sethulakshmi <sethukoodaranhi@gmail.com>
2025-08-29 20:15:45 +0530

    msg updated

--- schemas/contact.ts
+++ schemas/contact.ts
@@ -4,5 +4,5 @@ export const contactSchema = yup.object().shape({
   name: yup.string().required("Name is required").min(3, "Name must be at least 3 characters"),
   email: yup.string().required("Email is required").email("Invalid email format"),
   message: yup.string().required("Message is required").min(10, "Message must be at least 10 characters"),
-  subject: yup.string().required("Subject is required").min(5, "Subject must be at least 10 characters"),
+  subject: yup.string().required("Subject is required").min(5, "Subject must be at least 5 characters"),
 });
